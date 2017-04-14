552e7b0106a9a4267a06c68cb4f67de3f09568a5
github username <github email address>
2017-04-14 15:50:44 -0400

    finalllyyyyyyy

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,25 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+ def full_name
+    actor = Actor.create(first_name: first_name, last_name: last_name)
+    full_name = "#{first_name} #{last_name}"
+    full_name
+ end
+
+ def list_roles
+  array = []
+  character = self.characters.map {|char| char.name}
+  show = self.shows.map {|show| show.name}
+  array << character[0]
+  array << show[0]
+  array.join(" - ")
+
+  #  self.shows [#<Show:0x007ff456f4f298
+  # id: 1,
+  # name: "Game of Thrones",
+  # network_id: nil>]
+ end
+
 end
---
+++ db/migrate/007_add_a_catchphrase_to_characters.rb
@@ -0,0 +1,5 @@
+class AddACatchphraseToCharacters < ActiveRecord::Migration
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+end
