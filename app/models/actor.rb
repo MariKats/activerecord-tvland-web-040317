e93287256b87df0cfe997ad39072946c1e59a019
github username <github email address>
2017-04-14 15:57:44 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,18 +3,19 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
  def full_name
-    actor = Actor.create(first_name: first_name, last_name: last_name)
+    # actor = Actor.create(first_name: first_name, last_name: last_name)
     full_name = "#{first_name} #{last_name}"
     full_name
  end
 
  def list_roles
-  array = []
-  character = self.characters.map {|char| char.name}
-  show = self.shows.map {|show| show.name}
-  array << character[0]
-  array << show[0]
-  array.join(" - ")
+  # array = []
+  # character = self.characters.map {|char| char.name}
+  # show = self.shows.map {|show| show.name}
+  # array << character[0]
+  # array << show[0]
+  # array.join(" - ")
+  self.characters.map {|character| "#{character.name} - #{character.show.name}"}
 
   #  self.shows [#<Show:0x007ff456f4f298
   # id: 1,
